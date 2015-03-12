59ca72c41de3401ac03c109039b6656abb3c3df2
bvaleriv <bvaleriv@mail.ru>
2015-03-12 17:51:20 +0300

    Первое изменение

--- KursRabota/Program.cs
+++ KursRabota/Program.cs
@@ -14,6 +14,7 @@ namespace KursRabota
         [STAThread]
         static void Main()
         {
+            //kjhjkhkjh
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
